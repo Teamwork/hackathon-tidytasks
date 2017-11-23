be1a0847858eca5a98b266b3241724693cba3a5a
Alder Cass <alder.cass@teamwork.com>
2017-11-23 17:22:41 +0000

    Put auth stuff in a user class

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -1,49 +1,59 @@
-var apiKey, auth, getUserInfo;
+var User;
 
-apiKey = 'twp_i18f5vbTkrPN6TshHN85vcktq3Lo';
-
-auth = function(apiKey, callback) {
-  var opts;
-  if (typeOf(callback !== 'function')) {
-    callback = function() {};
+User = class User {
+  constructor() {
+    this.apiKey = 'twp_i18f5vbTkrPN6TshHN85vcktq3Lo';
+    this.domain = null;
+    this.userId = null;
+    this.userIcon = null;
   }
-  opts = {
-    method: 'GET',
-    beforeSend: function(xhr) {
-      var authHeader;
-      authHeader = 'Basic ' + btoa(apiKey + ':x');
-      return xhr.setRequestHeader('Authorization', authHeader);
-    },
-    crossDomain: true,
-    success: function(data) {
-      localStorage.setItem('auth', 'Basic ' + btoa(apiKey + ':x'));
-      localStorage.setItem('twurl', data.account.URL);
-      return callback();
-    },
-    error: function(err) {
-      return console.log(err);
-    }
-  };
-  $.ajax("https://authenticate.teamwork.com/authenticate.json", opts);
-};
 
-getUserInfo = function(callback) {
-  var opts;
-  if (typeOf(callback !== 'function')) {
-    callback = function() {};
-  }
-  opts = {
-    method: 'GET',
-    beforeSend: function(xhr) {
-      return xhr.setRequestHeader('Authorization', localStorage.getItem('auth'));
-    },
-    crossDomain: true,
-    success: function(data) {
-      return callback();
-    },
-    error: function(data) {
-      return console.log(data);
+  auth(apiKey, callback) {
+    var opts;
+    if (typeOf(callback !== 'function')) {
+      callback = function() {};
     }
-  };
-  $.ajax(localStorage.getItem('twurl') + "/me.json", opts);
+    opts = {
+      method: 'GET',
+      beforeSend: function(xhr) {
+        var authHeader;
+        authHeader = 'Basic ' + btoa(apiKey + ':x');
+        return xhr.setRequestHeader('Authorization', authHeader);
+      },
+      crossDomain: true,
+      success: function(data) {
+        localStorage.setItem('auth', 'Basic ' + btoa(apiKey + ':x'));
+        this.domain = data.account.URL;
+        this.userId = data.account.id;
+        this.userIcon = data.account['avatar-url'];
+        return callback();
+      },
+      error: function(err) {
+        return console.log(err);
+      }
+    };
+    $.ajax("https://authenticate.teamwork.com/authenticate.json", opts);
+  }
+
+  getUserInfo(callback) {
+    var opts;
+    if (typeOf(callback !== 'function')) {
+      callback = function() {};
+    }
+    opts = {
+      method: 'GET',
+      beforeSend: function(xhr) {
+        return xhr.setRequestHeader('Authorization', localStorage.getItem('auth'));
+      },
+      crossDomain: true,
+      success: function(data) {
+        return callback();
+      },
+      error: function(data) {
+        return console.log(data);
+      }
+    };
+    $.ajax(this.domain + "/me.json", opts);
+  }
+
 };
--- src/coffee/auth.coffee
+++ src/coffee/auth.coffee
@@ -1,39 +1,47 @@
-apiKey = 'twp_i18f5vbTkrPN6TshHN85vcktq3Lo'
+class User
 
-auth = (apiKey,callback) ->
-    if typeOf callback != 'function'
-        callback = ->
-            return
-    
-    opts = 
-        method: 'GET'
-        beforeSend: (xhr) ->
-            authHeader = 'Basic ' + btoa(apiKey + ':x')
-            xhr.setRequestHeader 'Authorization', authHeader
-        crossDomain: true
-        success: (data) ->
-            localStorage.setItem('auth', 'Basic ' + btoa(apiKey + ':x'))
-            localStorage.setItem('twurl', data.account.URL)
-            callback()
-        error: (err) ->
-            console.log(err)
+    constructor: ->
+        @apiKey = 'twp_i18f5vbTkrPN6TshHN85vcktq3Lo'
+        @domain = null
+        @userId = null
+        @userIcon = null
 
-    $.ajax "https://authenticate.teamwork.com/authenticate.json", opts
-    return
+    auth: (apiKey,callback) ->
+        if typeOf callback != 'function'
+            callback = ->
+                return
+        
+        opts = 
+            method: 'GET'
+            beforeSend: (xhr) ->
+                authHeader = 'Basic ' + btoa(apiKey + ':x')
+                xhr.setRequestHeader 'Authorization', authHeader
+            crossDomain: true
+            success: (data) ->
+                localStorage.setItem 'auth', 'Basic ' + btoa(apiKey + ':x')
+                @domain = data.account.URL
+                @userId = data.account.id
+                @userIcon = data.account['avatar-url']
+                callback()
+            error: (err) ->
+                console.log err
 
-getUserInfo = (callback) ->
-    if typeOf callback != 'function'
-        callback = ->
-            return
-    opts =
-        method: 'GET'
-        beforeSend: (xhr) -> 
-            xhr.setRequestHeader 'Authorization', localStorage.getItem('auth')
-        crossDomain: true
-        success: (data) ->
-            callback()
-        error: (data) ->
-            console.log data
+        $.ajax "https://authenticate.teamwork.com/authenticate.json", opts
+        return
 
-    $.ajax localStorage.getItem('twurl') + "/me.json", opts
-    return
+    getUserInfo: (callback) ->
+        if typeOf callback != 'function'
+            callback = ->
+                return
+        opts =
+            method: 'GET'
+            beforeSend: (xhr) -> 
+                xhr.setRequestHeader 'Authorization', localStorage.getItem('auth')
+            crossDomain: true
+            success: (data) ->
+                callback()
+            error: (data) ->
+                console.log data
+
+        $.ajax @domain + "/me.json", opts
+        return
